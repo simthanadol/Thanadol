ba8633490c1268291d8826cb82fd7d4ac3bf1ff6
simthanadol <54393445+simthanadol@users.noreply.github.com>
2019-08-22 15:33:18 +0700

    1

---
+++ test1.py
@@ -0,0 +1,8 @@
+print("Hello World")
+name = str(input("enter your name"))
+hello = "hello"+name
+print(hello)
+R = float(input("radias of circle"))
+Answer = 3.14*R**2
+print("Area is",Answer)
+print("Hello",input("Enter your name"))
